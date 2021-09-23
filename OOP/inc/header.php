e7bd23c6124af8d80dc74ec1a745a5e1d5c54221
Borhan Uddin <borhanuddin979@gmail.com>
2021-09-23 21:52:57 +0600

    changes

--- OOP/inc/header.php
+++ OOP/inc/header.php
@@ -1,52 +1,64 @@
 <!DOCTYPE HTML>
 <html>
+
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Object-Oriented PHP for Beginners</title>
+  <title>Object-Oriented PHP for Beginners - Wapborhan</title>
   <style>
-body {
-  font-family: arial;
-  font-size: 18px;
-  line-height: 22px;
-  margin: 0 auto;
-  width: 800px;
-}
-.headeroption {
-  background: #3399ff url("img/php.png") no-repeat scroll 56px 18px;
-  height: 80px;
-  overflow: hidden;
-  padding-left: 160px;
-}
-.footeroption{height:80px;background:#3399FF;overflow:hidden;}
-.headeroption h2 {
-  color: #000;
-  font-size: 30px;
-  padding-top: 5px;
-  text-shadow: 0 1px 1px #fff;
-}
-.footeroption h2 {
-  background: rgba(0, 0, 0, 0) url("img/logo.png") no-repeat scroll 65px 0;
-  color: #000;
-  font-size: 30px;
-  padding-bottom: 13px;
-  padding-top: 10px;
-  text-align: center;
-  text-shadow: 1px 1px 1px #fff;
-}
-.content {
-  background: #f2f2ff none repeat scroll 0 0;
-  border: 6px solid #3399ff;
-  font-size: 16px;
-  line-height: 22px;;
-  min-height: 420px;
-  overflow: hidden;
-  padding: 10px;
-}
+    body {
+      font-family: arial;
+      font-size: 18px;
+      line-height: 22px;
+      margin: 0 auto;
+      width: 800px;
+    }
+
+    .headeroption {
+      background: #3399ff url("img/php.png") no-repeat scroll 56px 18px;
+      height: 80px;
+      overflow: hidden;
+      padding-left: 160px;
+    }
+
+    .footeroption {
+      height: 80px;
+      background: #3399FF;
+      overflow: hidden;
+    }
+
+    .headeroption h2 {
+      color: #000;
+      font-size: 30px;
+      padding-top: 5px;
+      text-shadow: 0 1px 1px #fff;
+    }
+
+    .footeroption h2 {
+      background: rgba(0, 0, 0, 0) url("img/logo.png") no-repeat scroll 65px 0;
+      color: #000;
+      font-size: 30px;
+      padding-bottom: 13px;
+      padding-top: 10px;
+      text-align: center;
+      text-shadow: 1px 1px 1px #fff;
+    }
+
+    .content {
+      background: #f2f2ff none repeat scroll 0 0;
+      border: 6px solid #3399ff;
+      font-size: 16px;
+      line-height: 22px;
+      ;
+      min-height: 420px;
+      overflow: hidden;
+      padding: 10px;
+    }
   </style>
 </head>
+
 <body>
   <header class="headeroption">
     <h2>Object-Oriented PHP for Beginners</h2>
   </header>
-  
+
   <section class="content">
